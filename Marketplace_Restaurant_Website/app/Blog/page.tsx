1e81f14ed3267e46850efc5ea90e1de8a13940a4
Muhammad Sharjeel <m.sharjeelasif1435@gmail.com>
2025-01-29 13:30:14 +0500

    Blog Responsiveness

--- Marketplace_Restaurant_Website/app/Blog/page.tsx
+++ Marketplace_Restaurant_Website/app/Blog/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import BgPic from "@/app/Public/BgPic";
 import Header from "../Components/Header/Header"
 import BlogCard from "../Components/BlogComponent/BlogCard"
@@ -10,8 +10,10 @@ const Blog = () => {
 
       <BgPic PageHeading='Blog List' PageName='Blog' />
 
-      <BlogCard />
-
+      <Suspense fallback={<div>Loading...</div>}>
+        <BlogCard />
+      </Suspense>
+      
     </>
   )
 }
